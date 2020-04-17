c5e520a170acf5bdb1b1373f14424fe35df06ea7
Adam Altmark <adamaltmark@gmail.com>
2020-04-17 18:39:00 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,5 +1,5 @@
-board(" ", " ", " ", " ", " ", " ", " ", " ", " ")
-def display_board(board)
+answers(" ", " ", " ", " ", " ", " ", " ", " ", " ")
+def display_board(answers)
   puts " O |   |   "
   puts "-----------"
   puts "   | X |   "
